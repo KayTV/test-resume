e3d5e658fc2d97cfd42c407f4e16bcd0ba17d779
Kaylyn Van Norstrand <kaylyn.vannorstrand@gmail.com>
2016-05-13 20:24:32 -0600

    contact info completed

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,9 @@
 // add scripts
 $(document).on('ready', function() {
   console.log('sanity check!');
+
+  $('#poke_email').on('click', function(){
+    $('#new_poke').show();
+    $('#poke_email').hide();
+  })
 });
